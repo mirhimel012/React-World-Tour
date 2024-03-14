6fd76075718d150c4973a3f7d787afa2dd7a6d02
himelmia535 <himelmia535@gmail.com>
2024-03-14 20:44:51 +0600

    conditional style button

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import './Country.css';
 
 const Country = ({country}) => {
     const {name, capital, area, flags, cca3, region} = country;
 
+    const [visited, setVisited] = useState(false);
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
 
     return (
-        <div className="country">
-            <h2>Name: {name.common} </h2>
+        <div className={`country ${visited && 'visited'}`}>
+            <h2 style={{color: visited ? 'brown' : 'purple'}}>Name: {name.common} </h2>
             <h4>Official Name: {name.official} </h4>
             <h4>Capital: {capital} </h4>
             <h4>Area: {area} </h4>
@@ -15,6 +20,10 @@ const Country = ({country}) => {
             <div>
                 <img src={flags.png} alt="" />
             </div>
+
+            <button style={{marginRight:'6px'}} onClick={handleVisited}>{visited ? 'Visited':'Going'}</button>
+            {/* {visited && 'I have Visited This Country'} */}
+            {visited ? 'I have Visited This Country': 'I want to visit this Country'}
         </div>
     );
 };
